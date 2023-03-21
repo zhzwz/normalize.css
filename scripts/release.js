782f2db43fcd6ec16a1bc26a758c2d3ff5f6797c
zhzwz <zhaoziweizhi@gmail.com>
2023-03-21 17:29:26 +0800

    chore: change word publish to release

--- scripts/release.js
+++ scripts/release.js
@@ -7,22 +7,21 @@ const [, , version] = argv
 const command = promisify(exec)
 
 if (/^[\d]{1,}\.[\d]{1,}\.[\d]{1,}$/.test(version)) {
-  console.log(chalk.cyan(`Version: ${version}`))
   commit()
 } else {
   console.error(chalk.red(`Invalid version: ${version}`))
 }
 
 async function commit() {
-  const { stderr, stdout } = await command(`pnpm version ${version} -m "publish: version ${version}"`)
+  const { stderr, stdout } = await command(`pnpm version ${version} -m "release: version ${version}"`)
   if (stderr) console.error(chalk.red(stderr))
   if (stdout) {
-    console.log(stdout)
-    publish()
+    console.log(chalk.green(stdout))
+    release()
   }
 }
 
-async function publish() {
+async function release() {
   const { stderr, stdout } = await command('pnpm publish --access public')
   if (stderr) console.error(chalk.red(stderr))
   if (stdout) console.log(chalk.green(stdout))
